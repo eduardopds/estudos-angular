3e07892c0e4101cf54afdb7593c778f994241b94
Eduardoccufcg <eduardo.pereira.santos@ccc.ufcg.edu.br>
2020-05-01 15:37:49 -0300

    Tela de editar e botão de deletar

--- coronavairus-app/src/app/cases-details/cases-details.component.ts
+++ coronavairus-app/src/app/cases-details/cases-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Cases } from '../models/cases';
 @Component({
   selector: 'app-cases-details',
   templateUrl: './cases-details.component.html',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CasesDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
+
+  cases: Cases = { id: null, name: '', gender: '', age: null, address: '', city: '', country: '', status: '', created: null, updated: null };
+  isLoadingResults = true;
 
   ngOnInit(): void {
+    this.getCasesDetails(this.route.snapshot.params.id);
+
+  }
+  getCasesDetails(id : number){
+      this.api.getCasesById(id).subscribe((data:Cases) => {
+      this.cases = data;
+      console.log(this.cases);
+      this.isLoadingResults = false;
+    });
+  }
+
+  deleteCases(id: any) {
+    this.isLoadingResults = true;
+    this.api.deleteCases(id)
+      .subscribe(res => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/cases']);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
+
+  setColor(status: string) {
+    let color: string;
+    if (status === 'Positive') {
+      color = 'orange-color';
+    } else if (status === 'Recovered') {
+      color = 'green-color';
+    } else {
+      color = 'red-color';
+    }
+
+    return color;
   }
 
 }
--- coronavairus-app/src/app/edit-cases/edit-cases.component.ts
+++ coronavairus-app/src/app/edit-cases/edit-cases.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Cases } from '../models/cases';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-edit-cases',
@@ -7,9 +20,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCasesComponent implements OnInit {
 
-  constructor() { }
+  casesForm: FormGroup;
+  id = null
+  name: string = '';
+  gender: string = '';
+  age: number = null;
+  address: string = '';
+  city: string = '';
+  country: string = '';
+  status: string = '';
+  statusList = ['Positivo', 'Morto', 'Recuperado'];
+  genderList = ['Masculino', 'Feminino', 'Prefiro não dizer'];
+  isLoadingResults: boolean = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.getCasesById(this.route.snapshot.params.id);
+    this.casesForm = this.formBuilder.group({
+      name : ['' ,Validators.required],
+      gender : ['', Validators.required],
+      age : ['', Validators.required],
+      address : ['', Validators.required],
+      city : ['', Validators.required],
+      country : ['', Validators.required],
+      status : ['', Validators.required]
+    });
+  }
+
+  getCasesById(id: number) {
+    this.api.getCasesById(id).subscribe((data: Cases) => {
+      this.id = data.id;
+      this.casesForm.setValue({
+        name: data.name,
+        gender: data.gender,
+        age: data.age,
+        address: data.address,
+        city: data.city,
+        country: data.country,
+        status: data.status
+      });
+    });
+  }
+
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.api.updateCases(this.id, this.casesForm.value)
+      .subscribe((res: Cases) => {
+          const id = res.id;
+          this.isLoadingResults = false;
+          this.router.navigate(['/cases-details', id]);
+        }, (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
+
+  casesDetails() {
+    this.router.navigate(['/cases-details', this.id]);
   }
 
 }
